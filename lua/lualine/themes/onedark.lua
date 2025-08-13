d41d3c2b2e3f049db369a5f1489def47782e2b03
suzutsuki0114 <209620833+suzutsuki0114@users.noreply.github.com>
2025-08-13 20:06:12 +0900

    bの色を白にした

--- lua/lualine/themes/onedark.lua
+++ lua/lualine/themes/onedark.lua
@@ -5,28 +5,33 @@ local onedarkpro = {}
 
 onedarkpro.normal = {
     a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-    b = { bg = colors.fg_gutter, fg = colors.green },
+    -- b = { bg = colors.fg_gutter, fg = colors.green },
+    b = { bg = colors.fg_gutter, fg = colors.fg },
     c = { bg = config.options.lualine_transparency and colors.none or colors.bg_statusline, fg = colors.fg },
 }
 
 onedarkpro.insert = {
     a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-    b = { bg = colors.fg_gutter, fg = colors.blue },
+    -- b = { bg = colors.fg_gutter, fg = colors.blue },
+    b = { bg = colors.fg_gutter, fg = colors.fg },
 }
 
 onedarkpro.command = {
     a = { bg = colors.purple, fg = colors.bg, gui = "bold" },
-    b = { bg = colors.fg_gutter, fg = colors.purple },
+    -- b = { bg = colors.fg_gutter, fg = colors.purple },
+    b = { bg = colors.fg_gutter, fg = colors.fg },
 }
 
 onedarkpro.visual = {
     a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
-    b = { bg = colors.fg_gutter, fg = colors.yellow },
+    -- b = { bg = colors.fg_gutter, fg = colors.yellow },
+    b = { bg = colors.fg_gutter, fg = colors.fg },
 }
 
 onedarkpro.replace = {
     a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-    b = { bg = colors.fg_gutter, fg = colors.red },
+    -- b = { bg = colors.fg_gutter, fg = colors.red },
+    b = { bg = colors.fg_gutter, fg = colors.fg },
 }
 
 local inactive_bg = config.options.highlight_inactive_windows and colors.color_column or colors.bg
